48b303bc329d3784223402cf386c7a8bcfd0cbf8
HacenMario <stevenhacen@gmail.com>
2026-08-07 18:58:39 +0100

    Add CORS configuration to server.js
    Reordered the cors import and added configuration for specific origins and methods.

--- Pictures/ExpenseTracker/server/server.js
+++ Pictures/ExpenseTracker/server/server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const cors = require('cors');
 const path = require('path');
 const connectDB = require('./config/db');
 
 dotenv.config();
 connectDB();
+
+const cors = require('cors');
+  origin: 'https://expenses-manager-inky.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 const app = express();
 app.use(cors());
