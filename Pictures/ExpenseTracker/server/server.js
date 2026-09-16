098f573ce7a9bf2176bebfaa97a9e22655534cd5
HacenMario <stevenhacen@gmail.com>
2026-09-16 19:00:50 +0100

    Change server port from 5000 to 8080

--- Pictures/ExpenseTracker/server/server.js
+++ Pictures/ExpenseTracker/server/server.js
@@ -155,14 +155,14 @@ app.get('/api/status', (req, res) => {
     });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`🚀 Server running on http://expenses-manager-production-2169.up.railway.app:${PORT}`);
 });
 
 // ===== إبقاء الخادم نشطاً =====
 const keepAlive = () => {
-    const port = process.env.PORT || 5000;
+    const port = process.env.PORT || 8080;
     const url = `expenses-manager-production-2169.up.railway.app:${port}/api/status`;
     
     setInterval(async () => {
